3b7d535bea3c4340f5f4ca9725eb2e00d963ba90
Victor Santana <103072762+victorhosantana@users.noreply.github.com>
2022-04-11 12:04:27 +0100

    Inicio do game

--- UnoRacing1/UnoRacing1.py
+++ UnoRacing1/UnoRacing1.py
@@ -4,3 +4,16 @@ import pygame
 #Inicialização da biblioteca Pygame
 pygame.init()
 
+pygame.display.init()
+tela_ativa = True
+
+#Loop princiapl do jogo
+while tela_ativa:
+    pygame.display.set_mode((800, 600))
+    pygame.display.update()
+
+
+
+
+#Finalização do código
+pygame.quit()
